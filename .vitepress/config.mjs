2065f9eab4525443c0653ca5692c9e3c3fa0b905
4igurh <mlgigerchannel@gmail.com>
2024-08-07 22:58:08 +0300

    добавлен ax6s

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -16,7 +16,8 @@ export default defineConfig({
     darkModeSwitchLabel: "Оформление",
     returnToTopLabel: "Наверх",
     footer: {
-      message: "Используемые на данном сайте товарные знаки, логотипы и другие объекты интеллектуальной собственности принадлежат компании Keenetic и ее партнерам.<br>keen_prt не владеет никакими правами на указанные объекты, и вся информация на сайте представлена исключительно для личного пользования в ознакомительных целях.",
+      message:
+        "Используемые на данном сайте товарные знаки, логотипы и другие объекты интеллектуальной собственности принадлежат компании Keenetic и ее партнерам.<br>keen_prt не владеет никакими правами на указанные объекты, и вся информация на сайте представлена исключительно для личного пользования в ознакомительных целях.",
       copyright: "© 2024 keen_prt. Все права защищены.",
     },
     docFooter: {
@@ -34,17 +35,46 @@ export default defineConfig({
 
     sidebar: [
       {
-        text: "Examples",
+        text: 'Инструкции по установке',
+        collapsed: true,
         items: [
-          { text: "Markdown Examples", link: "/markdown-examples" },
-          { text: "Runtime API Examples", link: "/api-examples" },
+          { text: 'reserved', link: '/wiki/' },
+          { text: 'Xiaomi AX6S', link: '/wiki/guides/ax6s' },
+          { text: 'XG1 AX1800', link: '/config/three' },
+          { text: 'TP-Link EC330', link: '/config/four' },
+          { text: 'Netis N6 AX1800', link: '/config/four' },
+          { text: 'МТС WG430223', link: '/config/four' },
+          { text: 'SmartBox Flash', link: '/config/four' },
+          { text: 'SmartBox Giga', link: '/config/four' },
+          { text: 'SmartBox Pro', link: '/config/four' },
+          { text: 'SmartBox Turbo+', link: '/config/four' },
+          { text: 'WiFire NBN1500', link: '/config/four' },
+          { text: 'Xiaomi 4', link: '/config/four' },
+          { text: 'HLK7621', link: '/config/four' },
+          { text: 'Linksys EA7XXX', link: '/config/four' },
+          { text: 'Mercusys MR70X', link: '/config/four' },
         ],
       },
-    ],
+      {
+        text: 'FAQ',
+        collapsed: false,
+        items: [
+          { text: 'Обновление прошивок', link: '/config/' },
+          { text: 'Актуальный Breed', link: '/config/three' },
+          { text: 'BreedEnter', link: '/config/four' },
+          { text: 'Онлайн-конвертер EEPROM', link: '/config/four' },
+          { text: 'Тесты покрытия разных моделей', link: '/config/four' },
+          { text: 'Варианты удаленного доступа', link: '/config/four' },
+          { text: 'Мод для SmartBox Flash', link: '/config/four' },
+          { text: 'Мод для SmartBox Giga', link: '/config/four' },
+          { text: 'Дамп Breed для заливки через программатор', link: '/config/four' },
+          { text: 'Прошивка через MacOS', link: '/config/four' },
+        ],
+      }
+    ],    
 
     socialLinks: [
       {
-        
         icon: {
           svg: '<svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="50px" height="50px"><path d="M25,2c12.703,0,23,10.297,23,23S37.703,48,25,48S2,37.703,2,25S12.297,2,25,2z M32.934,34.375	c0.423-1.298,2.405-14.234,2.65-16.783c0.074-0.772-0.17-1.285-0.648-1.514c-0.578-0.278-1.434-0.139-2.427,0.219	c-1.362,0.491-18.774,7.884-19.78,8.312c-0.954,0.405-1.856,0.847-1.856,1.487c0,0.45,0.267,0.703,1.003,0.966	c0.766,0.273,2.695,0.858,3.834,1.172c1.097,0.303,2.346,0.04,3.046-0.395c0.742-0.461,9.305-6.191,9.92-6.693	c0.614-0.502,1.104,0.141,0.602,0.644c-0.502,0.502-6.38,6.207-7.155,6.997c-0.941,0.959-0.273,1.953,0.358,2.351	c0.721,0.454,5.906,3.932,6.687,4.49c0.781,0.558,1.573,0.811,2.298,0.811C32.191,36.439,32.573,35.484,32.934,34.375z"/></svg>',
         },
