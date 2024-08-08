651cd886f89a88bdae21c846a1573e60be7f1f81
4igurh <mlgigerchannel@gmail.com>
2024-08-08 09:48:32 +0300

    включение зума для отдельных картинок

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,17 +1,34 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import DefaultTheme from 'vitepress/theme'
-import './style.css'
+import { h } from "vue";
+import DefaultTheme from "vitepress/theme";
+import { onMounted, watch, nextTick } from "vue";
+import { useRoute } from "vitepress";
+import mediumZoom from "medium-zoom";
+import "./style.css";
 
 /** @type {import('vitepress').Theme} */
 export default {
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      mediumZoom("[data-zoomable]", { background: "var(--vp-c-bg)" });
+      // mediumZoom('.main img', { background: 'var(--vp-c-bg)' });
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    );
+  },
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
+    });
   },
   enhanceApp({ app, router, siteData }) {
     // ...
-  }
-}
+  },
+};
