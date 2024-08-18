ed8748f89f342f8e8fde7311bf0bc769b747fb9a
4igurh <mlgigerchannel@gmail.com>
2024-08-18 22:57:27 +0300

    Белый цвет иконки скачивания

--- .vitepress/components/YezBadge.vue
+++ .vitepress/components/YezBadge.vue
@@ -1,10 +1,11 @@
 <!-- components/YezBadge.vue -->
 <template>
-    <a :href="url" target="_blank" rel="noopener noreferrer" :class="`VPBadge ${type}`" style="text-decoration: none; color: inherit;">
+    <a :href="url" target="_blank" rel="noopener noreferrer" :class="`VPBadge ${type}`"
+        style="text-decoration: none; color: inherit;">
         {{ text }}
         <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g id="Interface / Download">
-                <path id="Vector" d="M6 21H18M12 3V17M12 17L17 12M12 17L7 12" stroke="#000000" stroke-width="2"
+                <path id="Vector" d="M6 21H18M12 3V17M12 17L17 12M12 17L7 12" stroke="var(--svg-color)" stroke-width="2"
                     stroke-linecap="round" stroke-linejoin="round" />
             </g>
         </svg>
@@ -23,6 +24,7 @@ defineProps({
 </script>
 <style scoped>
 .VPBadge.keenetic {
+    --svg-color: white;
     transform: translateY(0px);
     display: flex;
     color: white !important;
