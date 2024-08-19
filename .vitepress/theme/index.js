efa22d7a198182edb4842022a297195c4fbf9574
spatiumstas <79056064+spatiumstas@users.noreply.github.com>
2024-08-19 16:00:44 +0300

    Add RSA

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,7 +1,6 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from "vue";
+import { h, onMounted, watch, nextTick } from "vue";
 import DefaultTheme from "vitepress/theme";
-import { onMounted, watch, nextTick } from "vue";
 import { useRoute } from "vitepress";
 import mediumZoom from "medium-zoom";
 import "./style.css";
@@ -12,24 +11,47 @@ import YezBadgeWithDropdown from "../components/YezBadgeWithDropdown.vue";
 export default {
   setup() {
     const route = useRoute();
+
     const initZoom = () => {
       mediumZoom("[data-zoomable]", { background: "var(--vp-c-bg)" });
       mediumZoom(".main img", {
         background: "var(--vp-c-bg)",
       });
     };
+
     onMounted(() => {
       initZoom();
+
+      const script = document.createElement('script');
+      script.src = "https://yandex.ru/ads/system/context.js";
+      script.async = true;
+      document.head.appendChild(script);
+
+      script.onload = () => {
+        console.log("Yandex script loaded.");
+        window.yaContextCb.push(() => {
+          Ya.Context.AdvManager.render({
+            "blockId": "R-A-11653208-1",
+            "renderTo": "yandex_rtb_R-A-11653208-1"
+          });
+        });
+      };
+
+      script.onerror = () => {
+        console.error("Failed to load the Yandex script.");
+      };
     });
+
     watch(
-      () => route.path,
-      () => nextTick(() => initZoom())
+        () => route.path,
+        () => nextTick(() => initZoom())
     );
   },
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      "layout-top": () =>
+          h('div', { id: 'yandex_rtb_R-A-11653208-1' })
     });
   },
   enhanceApp({ app, router, siteData }) {
