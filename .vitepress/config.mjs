a0db259ecddd41e630f346b295fa0616fb47cdd4
spatiumstas <79056064+spatiumstas@users.noreply.github.com>
2024-12-28 13:46:20 +0300

    christmas time

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -69,7 +69,7 @@ export default defineConfig({
       next: false
     },
     sidebarMenuLabel: 'Меню',
-    logo: '/assets/images/logo.svg',
+    logo: '/assets/images/logo-ny.svg',
     siteTitle: false,
     // https://vitepress.dev/reference/default-theme-config
     nav: [
