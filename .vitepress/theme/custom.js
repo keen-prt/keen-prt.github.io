c394d69a9b7d77252ec679ea50e3dda39d2249be
spatiumstas <79056064+spatiumstas@users.noreply.github.com>
2025-01-24 20:24:30 +0300

    update texts

--- .vitepress/theme/custom.js
+++ .vitepress/theme/custom.js
@@ -5,42 +5,42 @@ if (typeof window !== 'undefined') {
         showPopup();
         localStorage.setItem('popup-shown', 'true');
       }
-  
+
       showCornerMessage();
     });
-  
+
     function showPopup() {
       const overlay = document.createElement('div');
       overlay.id = 'popup-overlay';
       overlay.classList.add('vp-doc');
-  
+
       const popup = document.createElement('div');
       popup.id = 'popup';
       popup.classList.add('vp-doc');
       popup.innerHTML = `
-        <p>У нас сменился чат и канал в Telegram. Обязательно подпишитесь на новый.<br><a href="https://t.me/keeneticported" target="_blank">Перейти в чат</a></p>
-        <button id="popup-close">Хорошо, подпишусь</button>
+        <p>У нас сменился Telegram-чат<br><a href="https://t.me/keeneticported" target="_blank">Перейти</a></p>
+        <button id="popup-close">Закрыть</button>
       `;
-  
+
       document.body.appendChild(overlay);
       document.body.appendChild(popup);
-  
+
       overlay.style.display = 'block';
       popup.style.display = 'block';
-  
+
       document.getElementById('popup-close').addEventListener('click', () => {
         popup.style.display = 'none';
         overlay.style.display = 'none';
       });
     }
-  
+
     function showCornerMessage() {
       const message = document.createElement('div');
       message.id = 'corner-message';
       message.classList.add('vp-doc');
       message.innerHTML = `
-        У нас сменился чат и канал в Telegram. Обязательно подпишитесь на новый.<br>
-        <a href="https://t.me/keeneticported" target="_blank">Перейти в чат</a>
+        У нас сменился Telegram-чат<br>
+        <a href="https://t.me/keeneticported" target="_blank">Перейти</a>
       `;
       document.body.appendChild(message);
     }
