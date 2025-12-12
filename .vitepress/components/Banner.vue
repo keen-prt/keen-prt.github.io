fd439460e1920ce06dd5fb6a5ada3d7fed74a8fc
keen-prt <110535306+keen-prt@users.noreply.github.com>
2025-12-12 21:08:56 +0300

    update text

--- .vitepress/components/Banner.vue
+++ .vitepress/components/Banner.vue
@@ -2,10 +2,8 @@
   <div class="banner">
     <a href="https://aeza.net/?ref=386645" target="_blank" rel="noopener">
       <img
-        src="https://io.aeza.net/partner-banners/90kagl.png"
-        alt="aéza ref link"
-        width="225"
-        height="450"
+        src="/assets/images/banner.jpg"
+        class="banner-img"
       />
     </a>
   </div>
